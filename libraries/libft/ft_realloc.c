31342a08d86b3b73aea98335ad0c95fb18e7fa15
Susanna Kayed <skayed@e3r1p5.42roma.it>
2025-10-16 14:33:22 +0200

    Add: custom realloc function

---
+++ libraries/libft/ft_realloc.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_realloc.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skayed <skayed@student.42roma.it>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/16 14:24:44 by skayed            #+#    #+#             */
+/*   Updated: 2025/10/16 14:28:41 by skayed           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_realloc(void *ptr, size_t size)
+{
+	void new_ptr;
+
+	if (!ptr)
+		return (malloc(size))
+	if ( size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	new_ptr = malloc(size);
+	if (!new_ptr)
+		return (NULL);
+	ft_memcpy(new_ptr, ptr, size);
+	free(ptr);
+	return (new_ptr);
+}
